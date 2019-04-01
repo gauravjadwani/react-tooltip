ab870964adc6c2781c30d183ed2f21439a04aed2
Gaurav <gaurav.jadwani@hotmail.com>
2019-04-02 00:26:19 +0530

    basic feature done

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 
 class Tooltip extends Component {
   render() {
+    console.log('props in this class',this.props);
     return (
       <div>
+        <div className={(this.props.parentClass)?(this.props.parentClass):("TooltipParentWrapper")}>
+          {this.props.childrenElement}
+          <span className={(this.props.childClass)?(this.props.childClass):("TooltipChildWrapper")}>{this.props.tooltipText}</span>
+        </div>;
       </div>
     );
   }
 }
 
-export default App;
+export default Tooltip;
